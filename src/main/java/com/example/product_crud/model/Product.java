a9f0648d74db9b67d782fdfd0be54148d8de7f83
cheikhtidiane03 <cheikhtidianeb764@gmail.com>
2026-05-06 12:58:49 +0000

    feat: Implementation des entity

--- src/main/java/com/example/product_crud/model/Product.java
+++ src/main/java/com/example/product_crud/model/Product.java
@@ -1,4 +1,24 @@
 package com.example.product_crud.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int categoryId;
+
+    public Product(int id, String name, double price, int categoryId) {
+        this.id         = id;
+        this.name       = name;
+        this.price      = price;
+        this.categoryId = categoryId;
+    }
+
+    public int    getId()          { return id; }
+    public String getName()        { return name; }
+    public double getPrice()       { return price; }
+    public int    getCategoryId()  { return categoryId; }
+
+    public void setName(String name)          { this.name = name; }
+    public void setPrice(double price)        { this.price = price; }
+    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
 }
